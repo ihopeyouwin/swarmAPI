c1cd5009b35eeace3227f44c7952f3ecfc83c8df
ivan <ihopeyouwon@gmail.com>
2020-01-14 12:35:42 +0300

    feat: places-users relations

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,24 +3,23 @@ const {validationResult} = require('express-validator');
 const User = require('../models/user');
 
 
-const Dummy_Users = [
-    {
-        id: 'u1', name: 'why why', email: 'test@test.com', password: '12345'
+const getUsers = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    } catch (err) {
+        return next(new HttpError('fetching users failed', 500));
     }
-];
-
-
-const getUsers = (req, res, next) => {
-    res.json({users: Dummy_Users})
+    res.json({users: users.map(user => user.toObject({getters: true})) })
 };
 
 const signUp = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return next(new HttpError('Invalid inputs passed, please check your data', 422))
+        return next(new HttpError('Invalid inputs passed, please check your data', 422))
     }
 
-    const {name, email, password, places} = req.body;
+    const {name, email, password} = req.body;
 
     let existingUser;
     try {
@@ -38,7 +37,7 @@ const signUp = async (req, res, next) => {
         email,
         image: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiG5uj8pYDnAhUCCewKHXXSCr4QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.freepik.com%2Ffree-icon%2Fuser-image-with-black-background_751548.htm&psig=AOvVaw2cUk2l-h7FPUNoYoH_aQRE&ust=1578995290761992',
         password,
-        places
+        places: []
     });
 
     try {
@@ -51,23 +50,23 @@ const signUp = async (req, res, next) => {
         return next(error)
     }
 
-    res.status(201).json({user: createdUser.toObject({ getters: true})});
+    res.status(201).json({user: createdUser.toObject({getters: true})});
 };
 
 
 const login = async (req, res, next) => {
     const {email, password} = req.body;
 
-  let existingUser;
-  try {
-    existingUser = await User.findOne({email: email})
-  } catch (err) {
-    return next(new HttpError('something went wrong, logging in failed', 500));
-  }
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email: email})
+    } catch (err) {
+        return next(new HttpError('something went wrong, logging in failed', 500));
+    }
 
 
     if (!existingUser || existingUser.password !== password) {
-      return next (new HttpError('could not identify user, credentials seem to be wrong or password is incorrect', 401))
+        return next(new HttpError('could not identify user, credentials seem to be wrong or password is incorrect', 401))
     }
     res.json({message: 'logged in'})
 };
