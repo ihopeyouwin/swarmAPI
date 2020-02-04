5c15d4531bc95b370229538639427010e32ff9e9
ivan <ihopeyouwon@gmail.com>
2020-02-04 14:37:57 +0300

    feat: multer to store files

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const usersControllers = require('../controllers/user-controller');
-const { check } = require('express-validator');
+const {check} = require('express-validator');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
-router.post('/signup', [
-  check('name').not().isEmpty(),
-  check('email').normalizeEmail().isEmail(),
-  check('password').isLength({ min: 6 })
-], usersControllers.signUp);
+router.post('/signup',
+  fileUpload.single('image'),
+  [
+      check('name').not().isEmpty(),
+      check('email').normalizeEmail().isEmail(),
+      check('password').isLength({min: 6})
+  ], usersControllers.signUp);
 router.post('/login', usersControllers.login);
 
 module.exports = router;
